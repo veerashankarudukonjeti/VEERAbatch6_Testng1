4d0d463a5c637b17889576d00300a2d838fa4629
veerashankarudukonjeti <100370638+veerashankarudukonjeti@users.noreply.github.com>
2022-09-12 20:38:51 +0530

    Update ReadConfig.java

--- src/main/java/utilities/ReadConfig.java
+++ src/main/java/utilities/ReadConfig.java
@@ -1,5 +1,43 @@
 package utilities;
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 public class ReadConfig {
 
-}
+		
+		public static Properties pro = new Properties();
+		
+		public static String ReadPropertiesData(String data) {
+			File f = new File(FilePath.propertiesPath);
+			FileInputStream fil = null;
+			try {
+				fil = new FileInputStream(f);
+			}
+			catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			try {
+				pro.load(fil);
+			} catch (IOException e) {
+				e.printStackTrace();
+				
+			}
+			
+			String dATA = pro.getProperty(data);
+			return dATA;
+		}
+		
+		public static String userName() {
+			String bname = ReadPropertiesData("username");
+			return bname;
+		}
+		
+		public static String passWord() {
+			String url = ReadPropertiesData("password");
+			return url;
+		}
+
+	}
+
