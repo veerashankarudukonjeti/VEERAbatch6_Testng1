15a0d69f2dfc202ce2aa5232d1670ce969047742
veerashankarudukonjeti <100370638+veerashankarudukonjeti@users.noreply.github.com>
2022-09-12 20:38:00 +0530

    Update ReadXL.java

--- src/main/java/utilities/ReadXL.java
+++ src/main/java/utilities/ReadXL.java
@@ -1,5 +1,56 @@
 package utilities;
 
-public class ReadXL {
+
+	
+
+	import java.io.File;
+	import java.io.FileInputStream;
+	import java.io.IOException;
+	import java.text.SimpleDateFormat;
+	import java.util.Date;
+
+	import org.apache.poi.ss.usermodel.Cell;
+	import org.apache.poi.ss.usermodel.DateUtil;
+	import org.apache.poi.ss.usermodel.Row;
+	import org.apache.poi.ss.usermodel.Sheet;
+	import org.apache.poi.ss.usermodel.Workbook;
+	import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+
+	public class ReadXL {
+		
+		public static String ReadXLData(String sheetName,int r,int c) throws IOException {
+			File F = new File(FilePath.xlString);
+					
+			FileInputStream fi;
+			String value = "";
+			
+			fi=new FileInputStream(F);
+			Workbook w = new XSSFWorkbook(fi);
+			Sheet s = w.getSheet(sheetName);
+			Row row = s.getRow(r);
+			Cell cell = row.getCell(c);
+			
+			int cellType = cell.getCellType();
+			
+			//cell is string
+			if(cellType==1) {
+				value = cell.getStringCellValue();
+			}
+				//cell is date
+			else if(DateUtil.isCellDateFormatted(cell)) {
+				Date d = cell.getDateCellValue();
+				SimpleDateFormat sim = new SimpleDateFormat("dd-MM-yyyy");
+				value = sim.format(d);
+			}
+			//cell is numeric
+			else {
+				double d = cell.getNumericCellValue();
+				long l = (long)d;
+				value = String.valueOf(1);
+			}
+			return value;
+		}
 
 }
