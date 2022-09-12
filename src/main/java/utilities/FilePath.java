22e2facecd7ef03b70fc132736b5ee5e94d2f651
veerashankarudukonjeti <100370638+veerashankarudukonjeti@users.noreply.github.com>
2022-09-12 20:39:54 +0530

    Update FilePath.java

--- src/main/java/utilities/FilePath.java
+++ src/main/java/utilities/FilePath.java
@@ -1,5 +1,10 @@
 package utilities;
 
-public class FilePath {
 
-}
+	public interface FilePath {
+		
+		public static String propertiesPath = "C:\\Users\\veera\\git\\repository\\TestNGHotelApp\\src\\test\\resources\\configFiles\\config.properties";
+		public static String xlString = "C:\\Users\\veera\\git\\repository\\TestNGHotelApp\\src\\test\\resources\\XLFiles\\TestNGHotelApp.xlsx";
+		
+
+	}
